7d46d627af501975d184421665b059b25c4a0e24
Leander Jedamus <ljedamus@web.de>
2020-12-10 16:34:56 +0100

    added color.ui = auto and pack.threads = 0

--- install_linux.sh
+++ install_linux.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env sh
 
 # erzeugt Donnerstag, 10. Dezember 2020 16:21 (C) 2020 von Leander Jedamus
-# modifiziert Donnerstag, 10. Dezember 2020 16:22 von Leander Jedamus
+# modifiziert Donnerstag, 10. Dezember 2020 16:33 von Leander Jedamus
 
 . ./install.sh
 git config --global credential.helper "/usr/local/bin/git-credential-gnome-keyring"
 #git config --global credential.helper store
+git config --global color.ui auto
+git config --global pack.threads 0
 
 cat gitalias.linux >> ~/.gitconfig
 
