52eb9d3d19351f797b16b995db11ab511c2d28b7
Leander Jedamus <ljedamus@web.de>
2020-12-10 16:27:01 +0100

    added install_macos.sh

---
+++ install_macos.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+# erzeugt Donnerstag, 10. Dezember 2020 16:23 (C) 2020 von Leander Jedamus
+# modifiziert Donnerstag, 10. Dezember 2020 16:23 von Leander Jedamus
+
+. ./install.sh
+
+cat gitalias.macos >> ~/.gitconfig
+
+# vim:ai sw=2
+
