fdde1bcdd3189bb2d6452cf4ab4e99b191cc5de1
Leander Jedamus <ljedamus@web.de>
2020-12-10 16:26:00 +0100

    updated gitinit.sh

--- gitinit.sh
+++ gitinit.sh
@@ -1,12 +1,23 @@
-#!/bin/sh
+#!/usr/bin/env sh
 
 # erzeugt Montag, 28. August 2017 13:21 (C) 2017 von Leander Jedamus
+# modifiziert Dienstag, 20. Oktober 2020 21:01 von Leander Jedamus
+# modifiziert Samstag, 26. September 2020 15:59 von Leander Jedamus
 # modifiziert Mittwoch, 13. September 2017 08:59 von Leander Jedamus
 # modifiziert Montag, 28. August 2017 13:26 von Leander Jedamus
 
-if [ -z $1 ]; then
+usage() {
   echo "usage: $0 <remote-repository on master>"
-  exit
+  exit 0
+};# usage()
+
+if [ -z $1 ]; then
+  usage
+else
+  case "$1" in
+    --help)     usage
+                ;;
+  esac
 fi
 
 git init
