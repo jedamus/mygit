1287b056638a22e71448fdb29b46db9729eb3d25
Leander Jedamus <ljedamus@web.de>
2020-12-10 16:36:55 +0100

    added echo

--- install.sh
+++ install.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env sh
 
 # erzeugt Donnerstag, 10. Dezember 2020 16:08 (C) 2020 von Leander Jedamus
-# modifiziert Donnerstag, 10. Dezember 2020 16:25 von Leander Jedamus
+# modifiziert Donnerstag, 10. Dezember 2020 16:36 von Leander Jedamus
 
 read -p "What is your name: " name
 echo "setting your name to $name"
 git config --global user.name $name
+echo ""
 
 read -p "What is your email-address: " email
 echo "setting your email-address to $email"
 git config --global user.email $email
+echo ""
 
 git config --global push.default simple
 
