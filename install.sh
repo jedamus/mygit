d873cabf07f426cc6d372a8dda828837c0bea332
Leander Jedamus <ljedamus@web.de>
2020-12-11 09:36:31 +0100

    added params for install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,24 @@
 #!/usr/bin/env sh
 
 # erzeugt Donnerstag, 10. Dezember 2020 16:08 (C) 2020 von Leander Jedamus
+# modifiziert Freitag, 11. Dezember 2020 09:35 von Leander Jedamus
 # modifiziert Donnerstag, 10. Dezember 2020 16:38 von Leander Jedamus
 
-read -p "What is your name: " name
-echo "setting your name to $name"
+if [ -z $2 ]; then
+  read -p "What is your name: " name
+  echo "setting your name to $name"
+  echo ""
+  
+  read -p "What is your email-address: " email
+  echo "setting your email-address to $email"
+  echo ""
+else
+  name=$1
+  email=$2
+fi
+
 git config --global user.name "$name"
-echo ""
-
-read -p "What is your email-address: " email
-echo "setting your email-address to $email"
 git config --global user.email "$email"
-echo ""
-
 git config --global push.default simple
 
 cp -vp gitinit.sh ~/bin
