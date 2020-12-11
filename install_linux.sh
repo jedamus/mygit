d27d124bb810d9b3c57a3a6267b03eeaa85102e5
Leander Jedamus <ljedamus@web.de>
2020-12-11 09:34:23 +0100

    removed install.sh from scripts

--- install_linux.sh
+++ install_linux.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 
 # erzeugt Donnerstag, 10. Dezember 2020 16:21 (C) 2020 von Leander Jedamus
+# modifiziert Freitag, 11. Dezember 2020 09:32 von Leander Jedamus
 # modifiziert Donnerstag, 10. Dezember 2020 16:33 von Leander Jedamus
 
-. ./install.sh
 git config --global credential.helper "/usr/local/bin/git-credential-gnome-keyring"
 #git config --global credential.helper store
 git config --global color.ui auto
--- install_macos.sh
+++ install_macos.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env sh
 
 # erzeugt Donnerstag, 10. Dezember 2020 16:23 (C) 2020 von Leander Jedamus
+# modifiziert Freitag, 11. Dezember 2020 09:33 von Leander Jedamus
 # modifiziert Donnerstag, 10. Dezember 2020 16:23 von Leander Jedamus
-
-. ./install.sh
 
 cat gitalias.macos >> ~/.gitconfig
 
