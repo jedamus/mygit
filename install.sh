40cbf4d7ddf2bd675fc7c6cf2d75847a89c2e413
Leander Jedamus <ljedamus@web.de>
2020-12-28 16:28:52 +0100

    minor changes

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env sh
 
 # erzeugt Donnerstag, 10. Dezember 2020 16:08 (C) 2020 von Leander Jedamus
+# modifiziert Montag, 28. Dezember 2020 16:28 von Leander Jedamus
 # modifiziert Freitag, 11. Dezember 2020 09:35 von Leander Jedamus
 # modifiziert Donnerstag, 10. Dezember 2020 16:38 von Leander Jedamus
 
-if [ -z $2 ]; then
+if [ -z "$2" ]; then
   read -p "What is your name: " name
   echo "setting your name to $name"
   echo ""
@@ -13,8 +14,8 @@ if [ -z $2 ]; then
   echo "setting your email-address to $email"
   echo ""
 else
-  name=$1
-  email=$2
+  name="$1"
+  email="$2"
 fi
 
 git config --global user.name "$name"
